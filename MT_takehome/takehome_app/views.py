62e498375e35b8e8b3ae64bbc9e8e5267a08e66d
Ian Livingston <ianlivingston@ians-mbp-2.mynetworksettings.com>
2024-03-09 12:08:14 -0700

    add views for FizzBuzzViewSet

--- MT_takehome/takehome_app/views.py
+++ MT_takehome/takehome_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render #unused
+from rest_framework import viewsets
+from .models import FizBuzz
+from .serializers import FizzBuzzSerializer
 
 # Create your views here.
+class FizzBuzzViewSet(viewsets.ModelViewSet):
+    queryset = FizzBuzz.objects.all()
+    serializer_class = FizzBuzzSerializer
