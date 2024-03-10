8d19433da8900b52ba3599ee55cb519956a91e5a
Ian Livingston <ianlivingston@ians-mbp-2.mynetworksettings.com>
2024-03-10 11:04:04 -0600

    'f' string to populate i into fizzparty fizzbuzz messages

--- MT_takehome/takehome_app/management/commands/fizzparty.py
+++ MT_takehome/takehome_app/management/commands/fizzparty.py
@@ -4,15 +4,20 @@ from takehome_app.models import FizzBuzz
 
 
 class Command(BaseCommand):
+    #message - what does this command do
     help = "get the FizzBuzz party started"
-
+    #args/kwargs are required here? operation doesn't error
+    #but fails to populate without those arguments
     def handle(self, *args, **kwargs):
+        #how many fizzbuzzes to create
         for i in range(100):
             FizzBuzz.objects.create(
+                #id and date generated automatically
                 useragent = 'fizzparty',
-                message = 'this is FizzBuzz {i} from the FizzParty'
+                #'f' before message indicates f-string (allows expressions inside strings)
+                message = f'this is FizzBuzz {i} from the FizzParty'
             )
-
+        #message that appears in command line if/when successfull
         self.stdout.write(
                     self.style.SUCCESS('100 FizzBuzzes have entered the room')
                 )       
